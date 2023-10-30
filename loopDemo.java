cef1b0599dc5642ff345c038d678645c3dd3ca70
Sevval Atalay <sevvalatalay7@gmail.com>
2023-10-30 23:27:53 +0300

    loop

---
+++ loopDemo.java
@@ -0,0 +1,23 @@
+public class loopDemo {
+    public static void main(String[] args) {
+        // for
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        // while
+        int i = 1;
+        while (i < 10) {
+
+            System.out.println(i);
+            i++;
+        }
+        // do-while
+        int j = 1;
+        do {
+            System.out.println(j);
+            j += 2;
+        } while (j < 10);
+
+    }
+
+}
