6208b72aa81fdf38fa6e426927a412f3b58f041a
Sevval Atalay <sevvalatalay7@gmail.com>
2023-12-05 19:59:26 +0300

    update

--- demo2.java
+++ demo2.java
@@ -5,9 +5,10 @@ public class demo2 {
         double tutar;
         double kdvOran = 0.18;
         System.out.println("tutar giriniz:");
-        Scanner inp = new Scanner(System.in);
-        tutar = inp.nextDouble();
-        System.out.println("tutariniz: " + tutar);
+        try (Scanner inp = new Scanner(System.in)) {
+            tutar = inp.nextDouble();
+            System.out.println("tutariniz: " + tutar);
+        }
 
         double miktar = tutar * kdvOran;
         System.out.println("kdv miktari: " + miktar);
