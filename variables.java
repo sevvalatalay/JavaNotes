c00bdc5963c31635ae68730d885ff273ca8c09ae
Sevval Atalay <sevvalatalay7@gmail.com>
2023-12-05 14:16:17 +0300

    variables

---
+++ variables.java
@@ -0,0 +1,18 @@
+/**
+ * variables
+ */
+public class variables {
+
+    public static void main(String[] args) {
+        float pi = 3.14F;
+        System.out.println(pi);
+        char vChar = 'b';
+        char v2Char = 98;
+        System.out.println(vChar);
+        System.out.println(v2Char);
+        System.out.println("" + vChar + v2Char);
+        boolean deneme = true;
+        System.out.println(deneme);
+
+    }
+}
