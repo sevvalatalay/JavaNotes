3ed7fb0cb8148cbdb4a80ee94a08066f701a28fe
Sevval Atalay <sevvalatalay7@gmail.com>
2023-12-05 19:32:20 +0300

    update

--- classScanner.java
+++ classScanner.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 
 public class classScanner {
     public static void main(String[] args) {
-        int a;
-        Scanner input = new Scanner(System.in);
-        // a = input.nextInt();
-        // System.out.println(a);
-        String str = input.nextLine();
-        System.out.println(str);
-        input.close();
+
+        try (Scanner input = new Scanner(System.in)) {
+            // a = input.nextInt();
+            // System.out.println(a);
+            String str = input.nextLine();
+            System.out.println(str);
+        }
+
     }
 
 }
